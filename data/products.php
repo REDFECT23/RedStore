bc894961cbb87651f9ad0ba0c497ee821b45f94f
REDFECT23 <ivanpoletaev684@gmail.com>
2025-03-09 01:11:31 +0700

    Create products.php

---
+++ data/products.php
@@ -0,0 +1,22 @@
+[
+  {
+    "name": "Виджет \"Таймер обратного отсчета\"",
+    "price": "$19.99",
+    "image": "/assets/img/product-timer.png"
+  },
+  {
+    "name": "Набор иконок \"Неон\"",
+    "price": "$9.99",
+    "image": "/assets/img/product-icons-neon.png"
+  },
+  {
+    "name": "Шаблон \"Лендинг для SaaS\"",
+    "price": "$49.99",
+    "image": "/assets/img/product-landing-saas.png"
+  },
+  {
+    "name": "Плагин \"Форма обратной связи\"",
+    "price": "$14.99",
+    "image": "/assets/img/product-form.png"
+  }
+]
